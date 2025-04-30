3b387191beb17882a6a36f9cd8faf259920f0dc0
Deffa Alaika <DAPradana@maybank.co.id>
2025-05-01 05:50:03 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.deffa.musicplayerapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.deffa.musicplayerapp"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("com.squareup.moshi:moshi:1.15.0")
+    // Moshi Kotlin module (adds Kotlin adapter factories)
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+    // (optional, but recommended) generate adapters at compile time
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.0")
+// optional, for loading artwork images:
+    implementation("io.coil-kt:coil:2.0.0")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}

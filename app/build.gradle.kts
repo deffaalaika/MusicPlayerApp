a15e2897e056ceedf90a706048e79febb115bb16
Deffa Alaika <DAPradana@maybank.co.id>
2025-05-01 19:54:14 +0700

    version 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.deffa.musicplayerapp"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
